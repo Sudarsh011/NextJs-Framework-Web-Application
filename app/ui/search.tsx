6b85ce4adf0af6a9241a845ef539a99a5a0d303c
Sudarsh Venkat Jeyaraman Rajesh <sudarsh011@sudarshs-air.home>
2024-01-30 14:38:01 -0500

    done till mutating data

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -7,7 +7,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
   function handleSearch(term: string) {
+    const handleSearch = useDebouncedCallback((term) => {
+      console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -15,7 +18,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     console.log(term);
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+}
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
